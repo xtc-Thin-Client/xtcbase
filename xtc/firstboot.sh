c7e129379538d250c8743a88272d2e137606cbae
Stephan Eisler <stephan@eisler.de>
2021-01-03 12:21:43 +0100

    Update firstboot.sh

--- xtc/firstboot.sh
+++ xtc/firstboot.sh
@@ -23,3 +23,41 @@ fi
 echo "System:" $OS
 echo "Model:" $MODEL
 
+echo "16. create data directory"
+mkdir $DATA 2>> /dev/null
+
+echo "17. mount data directory"
+if [ "$OS" = "R" ]
+then
+  cat /etc/fstab $DESTINATION/install/fstab_data > /tmp/fstab
+  mv /tmp/fstab /etc/fstab
+  mount -a
+fi
+
+echo "18. config data directory"
+chmod a=rwx $DATA 2>> /dev/null
+mkdir $DATA/thinclient
+chown thinclient $DATA/thinclient
+chgrp thinclient $DATA/thinclient
+mkdir $DATA/vpn
+chown thinclient $DATA/vpn
+chgrp thinclient $DATA/vpn 
+mkdir $DATA/usb
+chown thinclient $DATA/usb
+chgrp thinclient $DATA/usb
+
+echo "19. config read only fs"
+if [ "$OS" = "R" ]
+then
+  sed '/\/ /s/defaults/ro,defaults/g' -i /etc/fstab
+  sed '/\/boot/s/defaults/ro,defaults/g' -i /etc/fstab
+  cat /etc/fstab $DESTINATION/install/fstab_ro > /tmp/fstab
+  mv /tmp/fstab /etc/fstab
+fi
+
+echo "25. install software for argon1 case (Raspberry Pi 4 only)"
+if [ "$OS" = "R" ]
+then
+  $DESTINATION/desktop/argon1.sh
+  systemctl disable argononed
+fi
