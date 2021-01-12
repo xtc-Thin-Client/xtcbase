dac955bda16fd31f19ff9357d5e6a365663f1202
Stephan Eisler <stephan@eisler.de>
2021-01-12 12:34:21 +0100

    Update firstboot-late.sh

--- xtc/firstboot-late.sh
+++ xtc/firstboot-late.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
 SOURCE=/opt/thinclient/install
+
+rmdir /home/thinclient/Desktop
+rmdir /home/thinclient/Documents
+rmdir /home/thinclient/Downloads
+rmdir /home/thinclient/Music
+rmdir /home/thinclient/Pictures
+rmdir /home/thinclient/Public
+rmdir /home/thinclient/Templates
+
 sed '/\/ /s/defaults/ro,defaults/g' -i /etc/fstab
 sed '/\/boot/s/defaults/ro,defaults/g' -i /etc/fstab
 cat /etc/fstab $SOURCE/fstab_ro > /tmp/fstab
 mv /tmp/fstab /etc/fstab
 
+shutdown -r now
