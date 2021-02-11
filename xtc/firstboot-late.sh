6d85c2782d139c42580d85169512937b1d2c2d89
Stephan Eisler <stephan@eisler.de>
2021-02-11 20:48:11 +0100

    xtc 3

--- xtc/firstboot-late.sh
+++ xtc/firstboot-late.sh
@@ -2,15 +2,6 @@
 
 SOURCE=/opt/thinclient/install
 
-rm -rf /home/thinclient/Desktop
-rm -rf /home/thinclient/Documents
-rm -rf /home/thinclient/Downloads
-rm -rf /home/thinclient/Music
-rm -rf /home/thinclient/Pictures
-rm -rf /home/thinclient/Public
-rm -rf /home/thinclient/Templates
-rm -rf /home/thinclient/Videos
-
 sed '/\/ /s/defaults/ro,defaults/g' -i /etc/fstab
 sed '/\/boot/s/defaults/ro,defaults/g' -i /etc/fstab
 cat /etc/fstab $SOURCE/fstab_ro > /tmp/fstab
